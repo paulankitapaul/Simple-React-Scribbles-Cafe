b3fdb3a4468481f00081d3c224c37d0b24a05ec8
paulankitapaul <paulankita.ctg@gmail.com>
2025-04-08 21:34:47 +0600

    Json data loaded

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      
+      <Header></Header>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Blogs from '../Blogs/Blogs';
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className='font-bold text-4xl'> Knowledge-cafe</h2>
+            <Blogs></Blogs>
+        </div>
+    );
+};
+
+export default Header;
